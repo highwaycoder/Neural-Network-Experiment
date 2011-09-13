32351ea675fb76a17000586d7f342d4e827d6a25
Chris Browne <chris@pemcjd.me.uk>
2011-09-13 17:23:22 +0100

    updated TODO and got main.cpp compiling

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-#define SIZE_OF_POPULATION 4
+#include "Population.hpp"
+#include "RobotGenome.hpp"
+
+#define SIZE_OF_POPULATION 4U
 #define NUMBER_OF_GENERATIONS 50
 #define NUMBER_OF_TICKS 1000
 
 int main(void)
 {
-	Population pop = new Population(new RobotGenome(), SIZE_OF_POPULATION);
-	for(NUMBER_OF_GENERATIONS):
-		pop.simulate(NUMBER_OF_TICKS)
-		pop.nextGeneration()
+	int i=0;
+	Population pop = Population(RobotGenome(), SIZE_OF_POPULATION);
+	for(i=0;i<NUMBER_OF_GENERATIONS;i++)
+	{
+		pop.simulate(NUMBER_OF_TICKS);
+		pop.nextGeneration();
+	}
 	
-	RobotGenome g = pop.currentGenome()
+	// what is the below line for?
+	// RobotGenome g = pop.getCurrentGenome();
 	
 	return 0;
 }
